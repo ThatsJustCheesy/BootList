60a909b9eef759171fdc675d5bcb50a37e92d0f9
Ian Gregory <justcheesy@outlook.com>
2020-07-11 18:30:21 -0400

    Initial Commit

---
+++ BootList/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  BootList
+//
+//  Created by Ian Gregory on 11 Jul ’20.
+//  Copyright © 2020 Ian Gregory. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
